b9906ee034a7a8465f5393004a2cc60c803cbc9a
timvolzh <nigaman95@gmail.com>
2021-09-26 00:00:34 +0300

    Average grade fixed

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def average_grade(self):
+        amount = 0
+        amount_len = 0
+        for grades in self.grades.values():
+            amount += sum(grades)
+            amount_len += len(grades)
+        return amount / amount_len
+
     def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and (
                 course in self.courses_in_progress or course in self.finished_courses):
@@ -14,6 +22,33 @@ class Student:
                 lecturer.grades[course] += [grade]
             else:
                 lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        return f' Имя: {self.name}\n Фамилия: {self.surname}\n Средняя оценка: {self.average_grade():.1f}'
+
+    def __gt__(self, other):
+        if other in Student:
+            return self.average_grade > other.average_grade
+        else:
+            return 'Ошибка'
+
+    def __lt__(self, other):
+        if other in Student:
+            return self.average_grade < other.average_grade
+        else:
+            return 'Ошибка'
+
+    def __ge__(self, other):
+        if other in Student:
+            return self.average_grade >= other.average_grade
+        else:
+            return 'Ошибка'
+
+    def __le__(self, other):
+        if other in Student:
+            return self.average_grade <= other.average_grade
         else:
             return 'Ошибка'
 
@@ -41,6 +76,15 @@ class Lecturer(Mentor):
         self.courses_attached = []
         self.grades = {}
 
+    def average_grade(self, grades):
+        amount = 0
+        for grade in self.grades.values():
+            amount += sum(grade)
+        return amount / len(self.grades)
+
+    def __str__(self):
+        return f' Имя: {self.name}\n Фамилия: {self.surname}\n Средняя оценка: {self.average_grade():.1f}'
+
 
 class Reviewer(Mentor):
 
@@ -55,6 +99,9 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return f' Имя: {self.name}\n Фамилия: {self.surname}'
 
 
 # Students
@@ -75,7 +122,21 @@ obi_wan.courses_attached += ['English']
 
 # Reviewer
 minerva = Reviewer('Minerva', 'McGonagall')
+
 altiera = Reviewer('Altiera', 'Cunningham')
+
+
+# Test
+minerva.rate_student(paul, 'Python', 9)
+altiera.rate_student(paul, 'Python', 6)
+minerva.rate_student(paul, 'Python', 10)
+
+altiera.rate_student(liu, 'English', 7)
+minerva.rate_student(liu, 'English', 10)
+altiera.rate_student(liu, 'Python', 9)
+
+paul.rate_lecturer()
+
 
 
 # best_student = Student('Ruoy', 'Eman', 'your_gender')
